3a6f543b3fb4dcd415d402eaaf810debb9016618
kiku ishimoto <kikuish@gmail.com>
2020-04-15 02:59:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
     if (i === 0) {
       array.push('I am 1 strange loop')
     } else {
-      array.push(`I am ${arrray[i]} strange loops.`)
+      array.push(`I am ${i} strange loops.`)
     } }
   return array;
   }
