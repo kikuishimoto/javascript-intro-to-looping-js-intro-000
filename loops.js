c18f0a229e7407742b9f8240051199aff947b623
kiku ishimoto <kikuish@gmail.com>
2020-04-15 02:27:45 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+function forLoop(array) {
+  for (let i = 0; i <1; i++) {
+    console.log('I am 1 strange loop ')
+  }
+  for (let i = 1; i <25; i++) {
+
+    console.log(`I am ${i} strange loops.`)
+  }
+
+}
