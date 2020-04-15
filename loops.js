3a283c1048df9f071578066dced1f49cdff4fbb8
kiku ishimoto <kikuish@gmail.com>
2020-04-15 02:57:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,8 @@
 function forLoop(array) {
   for (let n = 0; i <25; n++) {
     if (n === 0) {
-      console.log('I am 1 strange loop')
+      array.push('I am 1 strange loop')
     } else {
-      console.log(`I am ${arrray[n]} strange loops.`)
+      array.push(`I am ${arrray[n]} strange loops.`)
     } }}
+    return array
